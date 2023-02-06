8ddbf9dabe3e72cbfeff188617cecc35bd6cfd37
ywkh <ywkh@fresnou.org>
2023-02-06 10:11:58 -0800

    initial commit

---
+++ datatypes.js
@@ -0,0 +1,76 @@
+Day 1 Notes:
+
+
+    console.log(7);
+
+// This is a comment
+
+
+
+// Numbers
+
+// Integers
+
+console.log(-3);
+
+console.log(0);
+
+console.log(17);
+
+// Floating Point Numbers
+
+console.log(3.14);
+
+console.log(100.045);
+
+
+
+//Strings
+
+//Characters, letters, symbols, and spaces
+
+console.log("hello world");
+
+console.log('Single quotes are okay, too!');
+
+console.log("Numbers can be part of a string 123");
+
+console.log(`You can also create a string using backticks`);
+
+
+
+
+
+// Arithmetic Operators (Math)
+
+console.log(14+2);
+
+console.log(14-2);
+
+console.log(14*2); //* means multiply
+
+console.log(14/2);
+
+console.log(14 % 2); //% is called MOD and means remainder after dividing
+
+console.log(15 % 10);
+Day 2 Notes:
+
+
+// Concatenation means to combine strings into a single string
+
+    console.log("My name "+"is Ms. Bolin.");
+
+console.log("My"+ " "+ "age"+ " "+ "is "+ 14);
+
+console.log("Your age is\n17");
+
+
+
+
+
+// Template literal – plug in some calculated value, then build the string on the fly
+
+//use backticks `` next to the 1
+
+console.log(`15 divided by 10 = ${15/10}`);
